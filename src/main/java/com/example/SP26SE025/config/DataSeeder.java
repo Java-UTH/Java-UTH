162cd24c7ac682ab33149fe71ce457d8df56690c
Đoàn Anh Tuấn <doananhtuan77qn@gmail.com>
2025-12-14 19:49:06 +0700

    Update DataSeeder.java

--- src/main/java/com/example/SP26SE025/config/DataSeeder.java
+++ src/main/java/com/example/SP26SE025/config/DataSeeder.java
@@ -22,13 +22,13 @@ public class DataSeeder implements CommandLineRunner {
 
         // NHỚ THAY ĐỔI CHỨC NĂNG CHO PHÙ HỢP VỚI WEB VÕNG MẠC
         createUserIfNotExists("admin@example.com", "123", Role.ADMIN);// QUẢN TRỊ VIÊN(ADMIN)
-        createUserIfNotExists("manager@example.com", "123", Role.CLINIC); // PHÒNG KHÁM
+        createUserIfNotExists("clinic@example.com", "123", Role.CLINIC); // PHÒNG KHÁM
         createUserIfNotExists("staff@example.com", "123", Role.DOCTOR); // BÁC SĨ
         createUserIfNotExists("customer@example.com", "123", Role.CUSTOMER);  // NGƯỜI DÙNG/ KHÁCH HÀNG
 
         // CỦA WEB CŨ 
         //createUserIfNotExists("admin@example.com", "123", Role.ADMIN);
-        //createUserIfNotExists("manager@example.com", "123", Role.MANAGER);
+        //createUserIfNotExists("clinic@example.com", "123", Role.CLINIC);
         //createUserIfNotExists("staff@example.com", "123", Role.STAFF);
         //createUserIfNotExists("consultant@example.com", "123", Role.CONSULTANT);
         //createUserIfNotExists("customer@example.com", "123", Role.CUSTOMER);
