c621f5f6b2fb1f35d962a8aab1b97487deef08a1
Đoàn Anh Tuấn <doananhtuan77qn@gmail.com>
2025-12-16 23:17:15 +0700

    Tuần 1

--- README.md
+++
@@ -1,1 +0,0 @@
-PHÒNG KHÁM , CLINIC
--- src/main/java/com/example/SP26SE025/repository/TestServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SP26SE025.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.SP26SE025.entity.TestService;
-
-public interface TestServiceRepository extends JpaRepository<TestService, Long> {
-}
