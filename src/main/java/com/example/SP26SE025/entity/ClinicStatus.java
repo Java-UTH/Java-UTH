909b8927e212c7ed02e5e907e043abd49dacf7f3
Nguyen Thanh Loc <loc08122004@gmail.com>
2026-01-12 16:10:46 +0700

    done ui clinic and  doing controllerclinic

---
+++ src/main/java/com/example/SP26SE025/entity/ClinicStatus.java
@@ -0,0 +1,8 @@
+package com.example.SP26SE025.entity;
+
+public enum ClinicStatus {
+    PENDING,
+    APPROVED,
+    SUSPENDED
+    
+}
---
+++ src/main/java/com/example/SP26SE025/repository/ClinicRepository.java
@@ -0,0 +1,9 @@
+package com.example.SP26SE025.repository;
+
+import com.example.SP26SE025.entity.Clinic;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClinicRepository extends JpaRepository<Clinic, Long> {
+
+    
+}
