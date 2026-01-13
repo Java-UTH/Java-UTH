aa915cbe8c666dd8f1f03bc0a42cb2dfe8ab8315
Nguyen Thanh Loc <loc08122004@gmail.com>
2026-01-13 10:49:28 +0700

    fix register user

--- src/main/java/com/example/SP26SE025/repository/ClinicRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SP26SE025.repository;
-
-import com.example.SP26SE025.entity.Clinic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClinicRepository extends JpaRepository<Clinic, Long> {
-
-    
-}
