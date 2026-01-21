9c4d57a7ac2e22609fe6601590c27779ff0d19d8
duocnguyen058 <dcnguyn058@gmail.com>
2026-01-21 21:05:42 +0700

    sửa lỗi up ảnh

--- src/main/java/com/example/SP26SE025/controller/CustomerController.java
+++ src/main/java/com/example/SP26SE025/controller/CustomerController.java
@@ -125,7 +125,7 @@ public class CustomerController {
     }
 
     private String uploadAvatar(MultipartFile file, Long userId) throws Exception {
-        String uploadDir = System.getProperty("user.dir") + "/target/classes/static/images/uploads";
+        String uploadDir = System.getProperty("user.dir") + "/src/main/resources/static/images/uploads";
         File uploadFolder = new File(uploadDir);
         if (!uploadFolder.exists())
             uploadFolder.mkdirs();
